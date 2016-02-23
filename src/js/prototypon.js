72bed10627443ff4a9dec7f6cef127eadfbf6035
abusedmedia <ff@todo.to.it>
2016-02-23 15:35:47 +0100

    added rebound helper

--- src/js/prototypon.js
+++ src/js/prototypon.js
@@ -4,6 +4,9 @@ window.Proto = {};
   
     var viewWidth = $(window).width()
     var viewHeight = $(window).height()
+    
+    var ratioW
+    var ratioH
 
     var width;
     var height;
@@ -16,16 +19,20 @@ window.Proto = {};
             
             svg = d3.select('svg');
 
-            width = svg.attr('width')
-            height = svg.attr('height')
+            width  = +svg.attr('width').split('px')[0]
+            height = +svg.attr('height').split('px')[0]
+            
             var view = svg.attr('viewBox').split(' ')
 
-            if(!width) width = view[2]
-            if(!height) height = view[3]
+            if(!width)  width  = +view[2]
+            if(!height) height = +view[3]
             
             // avoid this in iOS-7
             svg.attr('width', null)
                 .attr('height', null)
+            
+            ratioW = viewWidth/width
+            ratioH = viewHeight/height
             
             FastClick.attach(document.body);
 
@@ -77,6 +84,7 @@ window.Proto = {};
 
         var children = mask.selectAll('*')
         if(children.size() > 0){
+            children = mask.selectAll('g > *')
             children.each(function(d,i) { 
                 clippath.node()
                     .appendChild(this)
@@ -128,11 +136,104 @@ window.Proto = {};
 
         d3.select('body')
             .on('click', function(){
-            console.log('remove', selector);
                 couch.remove()
                 d3.select('body').on('click', null)
         })
     }
+
+    
+    
+    
+    
+    
+    function _Rebound(_options) {
+        
+        var options = {
+            source:_options.source,
+            steps:_options.steps,
+            view_size:_options.view_size,
+            direction:_options.direction,
+            update:_options.update
+        }
+                
+        var mapval = rebound.MathUtil.mapValueInRange
+        var springSystem = new rebound.SpringSystem()
+        var spring = springSystem.createSpring(50, 10)
+
+        var el = $(options.source)
+        var num = options.steps
+        var size = options.view_size //$(document).width();
+
+        var mousedown = false
+        var current = 0
+        var last = 0
+        var currentPos = 0
+        var currentShift = 0
+        var step = 0
+        var prp = (options.direction == 'x') ? 'pageX' : 'pageY'
+        var ratio = (options.direction == 'x') ? ratioW : ratioH
+        var prevLast
+
+        el.on('touchmousedown', function (e) {
+            var ev = e[prp] / ratio
+            current = ev
+            mousedown = true
+        });
+
+        $('body').on('touchmousemove', function (e) {
+            if (!mousedown) return;
+            
+            var ev = e[prp] / ratio
+
+            var fakt = 1;
+            if (step == 0 && ev > current) {
+                fakt = .19;
+            }
+            if (step == num-1 && ev < current) {
+                fakt = .19;
+            }
+
+            currentShift = (ev - current) * fakt;
+            
+            var v = (currentPos + currentShift);
+            var shiftNorm = mapval(v, 0, size * num, 0, 1)
+            options.update(v, shiftNorm*-1)
+            
+            spring.setCurrentValue(shiftNorm * -1);
+            
+            prevLast = last
+            last = ev;
+        })
+
+        $('body').on('touchmouseup', function (e) {
+            if (!mousedown) return;
+            mousedown = false;
+            currentPos += currentShift;
+
+            if (last < prevLast) {
+                step++;
+                if (step > num - 1) step = num - 1;
+            } else {
+                step--;
+                if (step < 0) step = 0;
+            }
+            spring.setEndValue(step / num);
+        })
+
+        spring.addListener({
+            onSpringUpdate: function (spring) {
+                var norm = spring.getCurrentValue();
+                val = mapval(norm, 0, 1, 0, (size * num * -1));
+                if (!mousedown) {
+                    options.update(val, norm)
+                    currentPos = val
+                }
+            }
+        });
+        
+    }
+
+
 
 
     Proto.clone = clone
@@ -142,6 +243,7 @@ window.Proto = {};
     Proto.height = viewHeight
     Proto.placeSVG = placeSVG
     Proto.clipBody = clipBody
+    Proto.Rebound = _Rebound
 
   
 })(window, window.Proto);
